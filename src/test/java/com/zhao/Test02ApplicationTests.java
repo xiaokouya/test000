88d493e9b78140521b3dff64be6bb85f3d085e1c
xiaozhao <zhaom10751@163.com>
2020-06-05 16:09:21 +0800

    v1.0.0

--- src/test/java/com/zhao/Test02ApplicationTests.java
+++ src/test/java/com/zhao/Test02ApplicationTests.java
@@ -1,13 +1,8 @@
 package com.zhao;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Test02ApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
